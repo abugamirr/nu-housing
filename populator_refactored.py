5e08bfee6474c79937061280ffc4239f4d5e9107
Alim Tleuliyev <tleuliyev.alim@gmail.com>
2023-07-19 11:43:20 +0600

    bug fix

--- populator_refactored.py
+++ populator_refactored.py
@@ -210,7 +210,7 @@ class Populator:
 		self.refresh_df_students_to_accommodate()
 		df = self.df_students_to_accommodate
 
-		df = df[(df['Gender'].isin(gender)) & (df['Degree'].isin(degree)) & (df['Year'].isin(year)) & df['Students'].apply(len).isin(num_of_roomates)]
+		df = df[(df['Gender'].isin(gender)) & (df['Degree'].isin(degree)) & (df['Year'].isin(year)) & (df['Intended Roommates'].apply(len).isin(num_of_roomates))]
 		
 		if ids is not None:
 			df = df[df['Id'].isin(ids)]
