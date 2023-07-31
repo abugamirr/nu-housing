d7b3a27c24f2187ede0a040ce1960ac4074fe9e3
Alim Tleuliyev <tleuliyev.alim@gmail.com>
2023-07-31 17:42:38 +0600

    Revert "Bug fix"
    This reverts commit 915afab20e5f6beb38cd0e4b6e498e4e8ea83d24.

--- populator_refactored.py
+++ populator_refactored.py
@@ -290,17 +290,18 @@ class Populator:
 			'Discount/Скидка'
 		]
 
-		upload_df = pd.DataFrame(columns = columns) #, index=range(len(accommodated_student_ids)))
+		upload_df = pd.DataFrame(columns = columns, index=range(len(accommodated_student_ids)))
 		index = 0
 		for block_num in self.dorm.rooms.keys():
 			for room in self.dorm.rooms[block_num].values():
 				for i in range(len(room.students)):
 					student = room.students[i]
+					
 					# UNCOMMENT THIS IF YOU WANT TO UPLOAD only students who were not in the dorm before
 					# if student.id not in accommodated_student_ids:
 					# 	continue
 					
-					upload_df.loc[len(upload_df.index)] = [student.id] + [np.nan] * 3 + [f'Блок {block_num}'] * 2 + [room.number] + [i + 1] + [np.nan] * 9
+					upload_df.iloc[index, :] = pd.Series([student.id] + [np.nan] * 3 + [f'Блок {block_num}'] * 2 + [room.number] + [i + 1] + [np.nan] * 9)
 					index += 1
 		
 		upload_df.to_excel('upload_file.xlsx', index=False)
